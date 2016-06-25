a6e4d65c45661793c368d3623ffa15b5d5346c5e
Jorge Henrique de Sousa Rocha <jhrocha.sousa@gmail.com>
2016-06-25 02:27:01 -0300

    Change attribute from cause

--- app/api/dont_buy/v1/entities/causes_statistics_entity.rb
+++ app/api/dont_buy/v1/entities/causes_statistics_entity.rb
@@ -1,6 +1,6 @@
 module DontBuy::V1::Entities
   class CausesStatisticsEntity < Grape::Entity
-    expose :group
+    expose :group, as: :group_title
     expose :causes, using: DontBuy::V1::Entities::DontBuyCauseEntity
   end
 end
